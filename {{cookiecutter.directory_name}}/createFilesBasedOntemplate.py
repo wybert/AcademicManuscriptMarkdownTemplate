a90592081eeeb7467aa20784ba448de7861cf68c
wybert <1072534112@qq.com>
2019-11-18 11:03:58 +0800

    修改一些bug

--- {{cookiecutter.directory_name}}/createFilesBasedOntemplate.py
+++ {{cookiecutter.directory_name}}/createFilesBasedOntemplate.py
@@ -1,9 +1,9 @@
 import os
 import yaml
 
-# outline = yaml.load(open('PHDThesis.yml',encoding="utf-8"))
+# outline = yaml.load(open('./outlines/PHDThesisOutlineTemplate.yml',encoding="utf-8"), Loader=yaml.FullLoader)
 
-outline = yaml.load(open('./outlines/academicManuscriptOutlineTemplate.yml',encoding="utf-8"))
+outline = yaml.load(open('./outlines/academicManuscriptOutlineTemplate.yml',encoding="utf-8"), Loader=yaml.FullLoader)
 
 
 for i,item in enumerate(outline):
@@ -11,7 +11,7 @@ for i,item in enumerate(outline):
         # print(type(item))
 
         if type(item) != type({}):
-            fileName = "./drafts/" + str(i) + "_" + item.replace(" ","_") + ".md"
+            fileName = "./drafts/" + str(i) + "_" + item.replace(" ","-") + ".md"
             with open(fileName,'w',encoding = "utf-8") as filehandler:
                 filehandler.write("## " + item)
                 filehandler.write("\n")
@@ -19,7 +19,7 @@ for i,item in enumerate(outline):
         else:
             for k,v in item.items():
                 # print(k,v)
-                fileName = "./drafts/" + str(i) + "_" + k.replace(" ","_") + ".md"
+                fileName = "./drafts/" + str(i) + "_" + k.replace(" ","-") + ".md"
                 with open(fileName,'w',encoding = "utf-8") as filehandler:
                     filehandler.write("## " + k)
                     filehandler.write("\n")
